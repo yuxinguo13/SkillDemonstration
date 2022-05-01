dc8ff57d49e0356a6844e34c550fa7cfe3b05183
Yuxin Guo <emily20021030@gmail.com>
2022-05-01 01:17:48 -0700

    add method

---
+++ SkillDemonstrationTest.java
@@ -0,0 +1,12 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstrationTest {
+    @Test
+    public void addition() {
+        assertEquals(10, SkillDemonstration.multiply(3, 4));
+    }
+    
+}
+
